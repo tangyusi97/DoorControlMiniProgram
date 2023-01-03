5aa4b8adb8afe7bb81813d5dadca1c65feb09543
Yusi Tang <2080466270@qq.com>
2023-01-03 14:09:16 +0800

    检查蓝牙不可用时，引导用户打开蓝牙

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -3,29 +3,46 @@
 const app = getApp<IAppOption>()
 
 Page({
-  data: {
-    disabled: {
-      open: false,
-      stop: false,
-      close: false,
-    }
-  },
+  data: {},
   onLoad() {
-    wx.openBluetoothAdapter({
-      mode: 'peripheral',
+    wx.showShareMenu({});
+    wx.onBluetoothAdapterStateChange(checkBluetootEnable);
+  },
+  onShow() {
+    checkBluetootEnable() && wx.authorize({
+      scope: 'scope.bluetooth',
       success() {
-        wx.createBLEPeripheralServer()
-          .then(res => app.globalData.BLEPeripheralServer = res.server);
-      },
-      fail(res) {
-        console.log(res);
-        wx.showModal({
-          title: '蓝牙初始化失败',
-          content: res.errMsg,
-          showCancel: false
+        wx.openBluetoothAdapter({
+          mode: 'peripheral',
+          success() {
+            wx.createBLEPeripheralServer()
+              .then(res => app.globalData.BLEPeripheralServer = res.server);
+          },
+          fail(res) {
+            console.log(res);
+            wx.showModal({
+              title: '蓝牙初始化失败',
+              content: res.errMsg,
+              showCancel: false
+            })
+          },
         })
       },
-    })
+      fail() {
+        wx.showModal({
+          title: '蓝牙授权失败',
+          content: "请前往设置页面开启蓝牙授权",
+          cancelText: "退出",
+          success(res) {
+            if (res.confirm) {
+              wx.openSetting();
+            } else if (res.cancel) {
+              wx.exitMiniProgram()
+            }
+          }
+        })
+      },
+    });
   },
   open() {
     controlling(app.globalData.BLEPeripheralServer, controlType.OPEN);
@@ -70,4 +87,30 @@ function controlling(server: WechatMiniprogram.BLEPeripheralServer | undefined, 
     }
   });
 
+}
+
+let modalVisible = false;
+
+function checkBluetootEnable(): Boolean {
+  if (!wx.getSystemSetting().bluetoothEnabled) {
+    if (!modalVisible) {
+      modalVisible = true;
+      wx.showModal({
+        title: "请打开蓝牙",
+        content: "请前往系统设置中打开蓝牙开关",
+        cancelText: "退出",
+        success(res) {
+          if (res.confirm) {
+            !wx.getSystemSetting().bluetoothEnabled &&
+              wx.openSystemBluetoothSetting();
+          } else if (res.cancel) {
+            wx.exitMiniProgram()
+          }
+          modalVisible = false;
+        }
+      });
+    }
+    return false;
+  }
+  return true;
 }
