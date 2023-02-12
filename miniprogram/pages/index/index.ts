6a8e696f053e024ee4ba0b59f69fc1a362586045
Yusi Tang <2080466270@qq.com>
2023-02-12 22:21:20 +0800

    增加在线修改参数功能

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -71,18 +71,38 @@ Page({
       success: res => {
         if (res.content == 'hkjmbzd') {
           sendCMD(app.globalData.BLEPeripheralServer, controlType.ENROLL);
-          setTimeout(this.cancel, 500);
+          setTimeout(this.cancel, 800);
           return;
         }
         if (res.cancel) return;
         wx.showToast({ title: '密码错误', icon: 'error' });
       }
     });
+  },
+  config() {
+    wx.showModal({
+      title: "设置iBeacon参数",
+      placeholderText: "请输入参数值：Enter,Debounce,Leave",
+      editable: true,
+      success: res => {
+        if (res.content.length == 6) {
+          const data: number[] = advertisingData[controlType.CONFIG];
+          data[13] = parseInt(res.content.substr(0, 2));
+          data[14] = parseInt(res.content.substr(2, 2));
+          data[15] = parseInt(res.content.substr(4, 2));
+          sendCMD(app.globalData.BLEPeripheralServer, controlType.CONFIG, data);
+          setTimeout(this.cancel, 800);
+          return;
+        }
+        if (res.cancel) return;
+        wx.showToast({ title: '参数不合法', icon: 'error' });
+      }
+    });
   }
 })
 
 
-enum controlType { OPEN, STOP, CLOSE, OPENCLOSE, ENROLL };
+enum controlType { OPEN, STOP, CLOSE, OPENCLOSE, ENROLL, CONFIG };
 
 const advertisingData: number[][] = [
   [0x6D, 0xB6, 0x43, 0x4F, 0x9E, 0x0F, 0x87, 0x91, 0x23, 0x6F, 0xCB, 0xCF, 0x65, 0xDA, 0x51, 0x3B],
@@ -90,16 +110,23 @@ const advertisingData: number[][] = [
   [0x6D, 0xB6, 0x43, 0x4F, 0x9E, 0x0F, 0x87, 0x91, 0x23, 0x6F, 0xCB, 0xCF, 0x65, 0x7A, 0x5B, 0x9E],
   [0x6D, 0xB6, 0x43, 0x4F, 0x9E, 0x0F, 0x87, 0x91, 0x23, 0x6F, 0xCB, 0xCF, 0x65, 0x87, 0x5E, 0xA4],
   [0x6D, 0xB6, 0x43, 0x4F, 0x9E, 0x0F, 0x87, 0x91, 0x23, 0x6F, 0xCB, 0xCF, 0x65, 0xC9, 0x54, 0x8D],
+  [0x6D, 0xB6, 0x43, 0x4F, 0x9E, 0x0F, 0x87, 0x91, 0x23, 0x6F, 0xCB, 0xCF, 0x6A],
 ]
 
-function sendCMD(server: WechatMiniprogram.BLEPeripheralServer | undefined, type: controlType) {
+function sendCMD(server: WechatMiniprogram.BLEPeripheralServer | undefined, type: controlType, data?: number[]) {
   if (!server) return;
   wx.vibrateShort({ type: 'heavy' });
+
+  let _data: ArrayBuffer = new Uint8Array(advertisingData[type]);
+  if (data) {
+    _data = new Uint8Array(data);
+  }
+
   server.startAdvertising({
     advertiseRequest: {
       manufacturerData: [{
         manufacturerId: "0xFFF0",
-        manufacturerSpecificData: new Uint8Array(advertisingData[type])
+        manufacturerSpecificData: _data
       }],
     },
     powerLevel: 'high',
